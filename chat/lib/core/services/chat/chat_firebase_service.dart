eb2e7a4775d2af1b036c34bbdc3fd6d88ab4277f
Caio Salvador Noboa <caiosalvador@hotmail.com>
2022-08-10 10:54:05 -0300

    refatorando-metodo-save-chat-firebase-service

---
+++ chat/lib/core/services/chat/chat_firebase_service.dart
@@ -0,0 +1,87 @@
+import 'dart:async';
+import 'package:chat/core/models/chat_user.dart';
+import 'package:chat/core/models/chat_message.dart';
+import 'package:chat/core/services/chat/chat_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ChatFirebaseService implements ChatService {
+  Stream<List<ChatMessage>> messagesStream() {
+    return Stream<List<ChatMessage>>.empty();
+  }
+
+  Future<ChatMessage?> save(String text, ChatUser user) async {
+    final store = FirebaseFirestore.instance;
+    final msg = ChatMessage(
+      id: '',
+      text: text,
+      createdAt: DateTime.now(),
+      userId: user.id,
+      userName: user.name,
+      userImageUrl: user.imageUrl,
+    );
+
+    final docRef = await store
+        .collection('chat')
+        .withConverter(
+          fromFirestore: _fromFirestore,
+          toFirestore: _toFirestore,
+        )
+        .add(msg);
+
+    // // Transformando ChatMessage em Map<String, dynamic> para podermos mandar para o firebase
+    // final docRef = await store.collection('chat').add({
+    //   'text': text,
+    //   'createdAt': DateTime.now().toIso8601String(),
+    //   'userId': user.id,
+    //   'userName': user.name,
+    //   'userImageUrl': user.imageUrl,
+    // });
+
+    // final doc = await docRef.get();
+    // final data = doc.data()!;
+
+    // // Transformando Map<String, dynamic> em ChatMessage para retornarmos
+    // return ChatMessage(
+    //   id: doc.id,
+    //   text: data['text'],
+    //   createdAt: DateTime.parse(data['createdAt']),
+    //   userId: data['userId'],
+    //   userName: data['userName'],
+    //   userImageUrl: data['userImageUrl'],
+    // );
+  }
+
+  // ChatMessage => Map<String, dynamic>
+  Map<String, dynamic> _toFirestore(
+    ChatMessage msg,
+    SetOptions? options,
+  ) {
+    return {
+      'text': msg.text,
+      'createdAt': DateTime.now().toIso8601String(),
+      'userId': msg.userId,
+      'userName': msg.userName,
+      'userImageUrl': msg.userImageUrl,
+    };
+  }
+
+  // Map<String, dynamic> => ChatMessage
+  ChatMessage _fromFirestore(
+    DocumentSnapshot<Map<String, dynamic>> doc,
+    SnapshotOptions? options,
+  ) {
+    return ChatMessage(
+      id: doc.id,
+      text: doc['text'],
+      createdAt: DateTime.parse(doc['createdAt']),
+      userId: doc['userId'],
+      userName: doc['userName'],
+      userImageUrl: doc['userImageUrl'],
+    );
+  }
+
+  //   .withConverter(
+  //   fromFirestore: fromFirestore,
+  //   toFirestore: toFirestore,
+  // )
+}
--- chat/lib/core/services/chat/chat_service.dart
+++ chat/lib/core/services/chat/chat_service.dart
@@ -1,7 +1,7 @@
 import 'package:chat/core/models/chat_message.dart';
 import 'package:chat/core/models/chat_user.dart';
-import 'package:chat/core/services/chat/chart_firebase_service.dart';
-import 'package:chat/core/services/chat/chart_mock_service.dart';
+import 'package:chat/core/services/chat/chat_firebase_service.dart';
+import 'package:chat/core/services/chat/chat_mock_service.dart';
 
 abstract class ChatService {
   // Ao trabalharmos com Stream com as mensagens, sempre que chegar uma nova mensagem, automaticamente receberemos esses valores
